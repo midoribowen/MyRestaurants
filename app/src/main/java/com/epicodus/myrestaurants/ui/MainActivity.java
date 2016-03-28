1a36362c0e371cd65a264df6b4e8d7e6f93d53f1
Midori Bowen <midori.p.bowen@gmail.com>
2016-03-27 17:52:44 -0700

    Add logout functionality.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.epicodus.myrestaurants.MyRestaurantsApplication;
 import com.epicodus.myrestaurants.R;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,10 +23,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private Firebase mFirebaseRef;
+
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
-
-    @Bind(R.id.loginButton) Button mLoginButton;
 
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
@@ -32,11 +37,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mFirebaseRef = MyRestaurantsApplication.getAppInstance().getFirebaseRef();
+        checkForAuthenticatedUser();
+
         mSharedPreferences = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
 
-        mLoginButton.setOnClickListener(this);
         mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                this.logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mFirebaseRef.unauth();
+        goToLoginActivity();
+    }
+
+    private void checkForAuthenticatedUser() {
+        AuthData authData = mFirebaseRef.getAuth();
+        if (authData == null) {
+            goToLoginActivity();
+        }
+    }
+
+    private void goToLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -44,16 +85,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.findRestaurantsButton:
                 String location = mLocationEditText.getText().toString();
-//                saveLocationToFirebase(location);
                 if (!(location).equals("")) {
                     addToSharedPreferences(location);
                 }
                 Intent findRestaurantsIntent = new Intent(MainActivity.this, RestaurantListActivity.class);
                 startActivity(findRestaurantsIntent);
-                break;
-            case R.id.loginButton:
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
                 break;
         }
     }
@@ -61,12 +97,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void addToSharedPreferences(String location) {
         mEditor.putString("location", location).commit();
     }
-
-//    public void saveLocationToFirebase(String location) {
-//        MyRestaurantsApplication.getAppInstance()
-//                .getFirebaseRef()
-//                .child("searchedLocations")
-//                .push()
-//                .setValue(location);
-//    }
 }
