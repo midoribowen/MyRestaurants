3f20bab3e8ccd7cdf0e4032a94f094a63b807061
Midori Bowen <midori.p.bowen@gmail.com>
2016-04-06 10:38:47 -0700

    Create landscape views for restaurant details.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantViewHolder.java
@@ -2,6 +2,9 @@ package com.epicodus.myrestaurants.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.epicodus.myrestaurants.ui.RestaurantDetailFragment;
 import com.epicodus.myrestaurants.util.ItemTouchHelperViewHolder;
 import com.squareup.picasso.Picasso;
 
@@ -27,9 +31,10 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder implements Vie
 
     @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
     @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
-    @Bind(R.id.categoryTextView) TextView mCategoryTextView;
-    @Bind(R.id.ratingTextView) TextView mRatingTextView;
+    @Bind(R.id.restaurantCuisineTextView) TextView mRestaurantCuisineTextView;
+    @Bind(R.id.restaurantRatingTextView) TextView mRestaurantRatingTextView;
     private Context mContext;
+    private int mOrientation;
     ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
     public RestaurantViewHolder(View itemView, ArrayList<Restaurant> restaurants) {
@@ -42,10 +47,21 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder implements Vie
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
-        intent.putExtra("position", getLayoutPosition() + "");
-        intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
-        mContext.startActivity(intent);
+        int currentPosition = getLayoutPosition();
+
+        if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            RestaurantDetailFragment detailFragment = RestaurantDetailFragment
+                    .newInstance(mRestaurants.get(currentPosition));
+            FragmentTransaction ft = ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.restaurantDetailContainer, detailFragment);
+            ft.commit();
+        } else {
+            Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+            intent.putExtra("position", getLayoutPosition() + "");
+            intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+            mContext.startActivity(intent);
+        }
+
     }
 
     public void bindRestaurant(Restaurant restaurant) {
@@ -56,8 +72,9 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder implements Vie
                 .into(mRestaurantImageView);
 
         mNameTextView.setText(restaurant.getName());
-        mCategoryTextView.setText(restaurant.getCategories().get(0));
-        mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        mRestaurantCuisineTextView.setText(restaurant.getCategories().get(0));
+        mRestaurantRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        mOrientation = itemView.getResources().getConfiguration().orientation;
     }
 
     @Override
